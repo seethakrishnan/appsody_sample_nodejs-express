37ba41873c00125b94109b05475b55e2cb024e15
seethakrishnan <seetha_krishnan@in.ibm.com>
2020-12-04 21:25:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Testing now latest with redhat image registry with project");
+  res.send("Hello from Appsody Testing now latest with redhat image registry with project modifed image location");
 });
  
 module.exports.app = app;
