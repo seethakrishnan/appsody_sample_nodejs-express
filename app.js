8c3f0eca6c8a8e592218e0156a9ea5c854236b5e
seethakrishnan <seetha_krishnan@in.ibm.com>
2020-12-04 20:24:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Testing now latest....!");
+  res.send("Hello from Appsody Testing now latest with docker");
 });
  
 module.exports.app = app;
