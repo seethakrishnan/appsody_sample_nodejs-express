41ff38d48ed7cf474bccdd1c9cd280273303a2d1
seethakrishnan <seetha_krishnan@in.ibm.com>
2020-12-07 16:00:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Testing now latest with redhat image registry with only namespace and removed appname122");
+  res.send("Hello from Appsody Testing now latest with redhat image registry with only namespace and removed appname123");
 });
  
 module.exports.app = app;
